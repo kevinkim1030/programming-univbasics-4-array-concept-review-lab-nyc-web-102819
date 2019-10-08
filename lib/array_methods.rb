03984dc1b00d4d241f2733374556a1a848f5985e
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-08 01:55:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,10 +4,10 @@ end
 
 def find_max_value(array)
   array.sort 
-  array.pop
+  find_max_value = array.pop
 end
 
 def find_min_value(array)
   array.sort
-  array.push
+  find_min_value = array.push
 end
