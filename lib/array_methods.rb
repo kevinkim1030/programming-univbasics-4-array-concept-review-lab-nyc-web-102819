04afab4a638ea5910f85f5b039268f8eae5beb2b
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-08 01:56:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort 
-  find_max_value = array.pop
+  find_max_value = array.sort.pop
 end
 
 def find_min_value(array)
